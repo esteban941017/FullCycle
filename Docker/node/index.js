0486bb452b640bbdbbf8b4544459c00177106ea0
Esteban Ramirez <esteban941017@gmail.com>
2024-08-19 22:43:14 -0300

    added docker compose, node and mysql database

--- Docker/node/index.js
+++ Docker/node/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
+};
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name)
+VALUES('Bernardo')`;
+
+connection.query(sql);
+connection.end();
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>');
